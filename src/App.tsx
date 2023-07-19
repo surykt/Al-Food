1fe17e6c18606258c3b51c963eeec4887959a2de
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-19 17:20:14 -0300

    create admin page to Al-food project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './paginas/Home';
-import VitrineRestaurantes from './paginas/VitrineRestaurantes';
+import { Routes, Route } from "react-router-dom";
+import Home from "./paginas/Home";
+import VitrineRestaurantes from "./paginas/VitrineRestaurantes";
+import AdministraçãoRestaurantes from "./paginas/Admin/Restaurantes/AdministracaoRestaurantes";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/restaurantes" element={<VitrineRestaurantes />} />
+      <Route
+        path="/admin/restaurantes"
+        element={<AdministraçãoRestaurantes />}
+      />
     </Routes>
   );
 }
---
+++ src/paginas/Admin/Restaurantes/AdministracaoRestaurantes.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export default function AdministraçãoRestaurantes() {
+  const [restaurantes, setRestaurantes] = useState();
+
+  return <h1>Adm restaurantes</h1>;
+}
