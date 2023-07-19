19447e49c05d4563c634eca23614e236e59adbea
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-19 14:45:40 -0300

    add restaurant list paginaton request

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -3,18 +3,35 @@ import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
 import axios from "axios";
+import { IPaginacao } from "../../interfaces/IPaginacao";
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+  const [proximaPagina, setProximaPagina] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:8000/api/v1/restaurantes/").then(resposta => {
-      setRestaurantes(resposta.data.results);
-    });
+    axios
+      .get<IPaginacao<IRestaurante>>(
+        "http://localhost:8000/api/v1/restaurantes/",
+      )
+      .then(resposta => {
+        setRestaurantes(resposta.data.results);
+        setProximaPagina(resposta.data.next);
+      });
     // .catch(erro => {
     //   console.log(erro)
     // })
   }, []);
+
+  const verMais = () => {
+    axios.get<IPaginacao<IRestaurante>>(proximaPagina).then(resposta => {
+      setRestaurantes([...restaurantes, ...resposta.data.results]);
+      setProximaPagina(resposta.data.next);
+    });
+    // .catch(erro => {
+    //   console.log(erro)
+    // })
+  };
 
   return (
     <section className={style.ListaRestaurantes}>
@@ -24,6 +41,7 @@ const ListaRestaurantes = () => {
       {restaurantes?.map(item => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
+      {proximaPagina && <button onClick={verMais}>Ver mais</button>}
     </section>
   );
 };
