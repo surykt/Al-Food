88fa2db7ca2e8848d9d83ee969eb4431b14478aa
Carlos Alan <carlos.cardoso.meta@youse.com.br>
2023-07-28 15:22:06 -0300

    serch restaurant

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -2,16 +2,22 @@ import { useEffect, useState } from "react";
 import IRestaurante from "../../interfaces/IRestaurante";
 import style from "./ListaRestaurantes.module.scss";
 import Restaurante from "./Restaurante";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { IPaginacao } from "../../interfaces/IPaginacao";
 
 const ListaRestaurantes = () => {
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
   const [proximaPagina, setProximaPagina] = useState("");
   const [paginaAnterior, setPaginaAnterior] = useState("");
+  const [busca, setBusca] = useState("");
 
-  const carregarDados = (url: string) => {
-    axios.get<IPaginacao<IRestaurante>>(url).then(resposta => {
+  interface IParametrosBusca {
+    ordering?: string;
+    search?: string;
+  }
+
+  const carregarDados = (url: string, opcoes: AxiosRequestConfig = {}) => {
+    axios.get<IPaginacao<IRestaurante>>(url, opcoes).then(resposta => {
       setRestaurantes(resposta.data.results);
       setProximaPagina(resposta.data.next);
       setPaginaAnterior(resposta.data.previous);
@@ -20,6 +26,17 @@ const ListaRestaurantes = () => {
     //   console.log(erro)
     // })
   };
+
+  function buscar(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    const opcoes = {
+      params: {} as IParametrosBusca,
+    };
+    if (busca) {
+      opcoes.params.search = busca;
+    }
+    carregarDados("http://localhost:8000/api/v1/restaurantes/", opcoes);
+  }
 
   useEffect(() => {
     carregarDados("http://localhost:8000/api/v1/restaurantes/");
@@ -30,6 +47,14 @@ const ListaRestaurantes = () => {
       <h1>
         Os restaurantes mais <em>bacanas</em>!
       </h1>
+      <form onSubmit={buscar}>
+        <input
+          type="text"
+          value={busca}
+          onChange={evento => setBusca(evento.target.value)}
+        />
+        <button type="submit">buscar</button>
+      </form>
       {restaurantes?.map(item => (
         <Restaurante restaurante={item} key={item.id} />
       ))}
--- src/paginas/Admin/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Admin/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import IRestaurante from "../../../interfaces/IRestaurante";
 import {
+  Button,
   Paper,
   Table,
   TableBody,
